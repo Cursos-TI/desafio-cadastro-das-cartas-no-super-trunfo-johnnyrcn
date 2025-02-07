ae7e86d493339f6114c901e3ba0764820c6a7030
johnnyrcn <99361737+johnnyrcn@users.noreply.github.com>
2025-02-07 03:41:57 +0000

    Criei o sistema básico do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,39 @@
 //Teste larissa
 
 int main() {
+    char cidade[30];
+    int codigo;
+    int populacao;
+    float area;
+    float PIB;
+    int pts_turisticos;
+
+    printf("Insira o nome da cidade: ");
+    scanf("%s", &cidade);
+
+    printf("Insira o código da cidade: ");
+    scanf("%d", &codigo);
+
+    printf("Insira o tamanho da população: ");
+    scanf(" %d", &populacao);
+
+    printf("Insira o tamanho da área: ");
+    scanf("%f", &area);
+
+    printf("Insira o PIB: ");
+    scanf("%f", &PIB);
+
+    printf("Insira a quantidade de pontos turísticos: ");
+    scanf("%d", &pts_turisticos);
+
+    printf("Cidade: %s\n", cidade);
+    printf("Código da cidade: %d\n", codigo);
+    printf("Tamanho da população: %d\n", populacao);
+    printf("Tamanho da área: %f\n", area);
+    printf("PIB da cidade: %f\n", PIB);
+    printf("Quantidade de pontos turísticos: %d\n", pts_turisticos);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
